0bc7d854e60f0290f1d43fd6b002fe90dc2729bd
KitchenTable99 <ccbitt23@colby.edu>
2023-03-06 16:22:37 -0500

    rename needed columns

---
+++ user_twarc_prep.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def main():
+    twarc_df = pd.read_csv('dev_users.csv')
+    twarc_df = twarc_df.rename(columns={
+        'public_metrics.following_count': 'friends_count',
+        'public_metrics.followers_count': 'followers_count',
+        'public_metrics.listed_count': 'listed_count',
+        })
+    twarc_df.to_csv('prepped.csv', index=False)
+
+if __name__ == "__main__":
+    main()
