c8fd42602c5e923a29e14d1b057eb10139762543
KitchenTable99 <ccbitt23@colby.edu>
2023-03-29 11:13:56 -0400

    add df -> sql for tweet features

---
+++ into_db.py
@@ -0,0 +1,35 @@
+import os
+import logging
+import pickle
+import pandas as pd
+import sqlite3
+
+
+with open('./column_order.pickle', 'rb') as fp:
+    COLUMN_ORDER = pickle.load(fp)
+
+
+def get_conn(testing=True):
+    return sqlite3.connect(':memory:') if testing else sqlite3.connect('network_features.db')
+
+
+def insert_into_db(file, conn):
+    logging.info(f'{file = }')
+    df = pd.read_parquet(file)
+    table_name = 'like_features' if 'likes' in file else 'retweet_features'
+    df.to_sql(table_name, conn, if_exists='append')
+
+
+def main():
+    conn = get_conn(testing=False)
+    to_read = os.listdir('./processed_tweets/')
+    for file in to_read:
+        if file == 'tiny.zip':
+            continue
+        insert_into_db(f'processed_tweets/{file}', conn)
+    conn.close()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
